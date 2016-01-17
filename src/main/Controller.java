50f58acb29addf4cf6e7ed1f359bb5110aec75e8
GitHhb <github@mittere.net>
2016-01-17 05:17:35 +0100

    Initial commit

---
+++ src/main/Controller.java
@@ -0,0 +1,55 @@
+package main;
+
+import javafx.event.Event;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TextField field0;
+    public TextField field1;
+    public TextField field2;
+    public TextField field3;
+    public TextField field4;
+    public TextField field5;
+    public TextField field6;
+    public TextField field7;
+    public TextField field8;
+
+    public void onMouseClick0(Event event) {
+        field0.setText("X");
+    }
+
+    public void onMouseClick1(Event event) {
+        field1.setText("X");
+    }
+
+    public void onMouseClick2(Event event) {
+        field2.setText("X");
+    }
+
+    public void onMouseClick3(Event event) {
+        field3.setText("X");
+    }
+
+    public void onMouseClick4(Event event) {
+        field4.setText("X");
+    }
+
+    public void onMouseClick5(Event event) {
+        field5.setText("X");
+    }
+
+    public void onMouseClick6(Event event) {
+        field6.setText("X");
+    }
+
+    public void onMouseClick7(Event event) {
+        field7.setText("X");
+    }
+
+    public void onMouseClick8(Event event) {
+        field8.setText("X");
+    }
+
+}
